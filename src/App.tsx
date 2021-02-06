f271e4169a280b318b1637159fb016d698f4c116
Gabriel von Platen <gabrielvonplaten@gmail.com>
2021-02-06 19:00:30 +0100

    Further cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
 import './App.global.css';
 
 const Hello = () => {
   return (
     <div>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <h1>Hello</h1>
     </div>
   );
 };
